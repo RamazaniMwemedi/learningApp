a253d965c6a82ef09ab73aa1b72fc82c32f5e01b
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-06-11 14:16:07 +0300

    Added the Topic Component and styled it

--- kidsapp/src/Components/Home.js
+++ kidsapp/src/Components/Home.js
@@ -4,19 +4,96 @@ const Home = () => {
   return (
     <div className="home">
       <h1>This is Home page</h1>
-        <MidHome />
+      <MidHome />
     </div>
   );
 };
 
 export default Home;
 
-const MidHome = () => { 
-    const buttonsArray = []
-    return (
-        <div className="mid-home">
-            <h1>This is Mid Home page</h1>
-        </div>
-    );
+const MidHome = () => {
+  const buttonsArray = [
+    {
+      name: "Alphabets",
+      color: "green",
+      id: 1,
+    },
+    {
+      name: "Shapes",
+      color: "yellow",
+      id: 2,
+    },
+    {
+      name: "Colors",
+      color: "purple",
+      id: 3,
+    },
+    {
+      name: "Days",
+      color: "wheat",
+      id: 10,
+    },
+    {
+      name: "Months",
+      color: "blue",
+      id: 4,
+    },
+    {
+      name: "Animals",
+      color: "red",
+      id: 5,
+    },
+    {
+      name: "Fruits",
+      color: "orange",
+      id: 6,
+    },
+    {
+      name: "Parts of the body",
+      color: "brown",
+      id: 7,
+    },
+    {
+      name: "Arithmetic",
+      color: "pink",
+      id: 8,
+    },
+    {
+      name: "Parts of a Computer",
+      color: "dodgerblue",
+      id: 9,
+    },
+    {
+      name: "",
+      color: "dodgerblue",
+      id: 11,
+    },
+    {
+      name: "",
+      color: "dodgerblue",
+      id: 12,
+    },
+  ];
+  return (
+    <div className="mid-home">
+      <div className="buttons-container">
+        {buttonsArray.map((object) => (
+          <Topic key={object.id} object={object} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
- }
+const Topic = ({ object }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: object.color,
+      }}
+        className="topic"
+    >
+      <h1>{object.name}</h1>
+    </div>
+  );
+};
