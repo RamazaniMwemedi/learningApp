71d83d511b04923bacedda1c163d07aca3d39d31
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-06-11 17:02:48 +0300

    Added Images in left footer

--- kidsapp/src/Components/Footer.js
+++ kidsapp/src/Components/Footer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "../styles/Footer.css";
+
+import facebooklogo from "../Images/facebooklogo.png";
+import instagramlogo from "../Images/instagramlogo.png";
+import gmaillogo from "../Images/gmaillogo.png";
+import linkedinlogo from "../Images/linkedinlogo.png";
+
 const Footer = () => {
+  const icons = [facebooklogo, instagramlogo, gmaillogo, linkedinlogo];
   return (
     <div className="footer">
       <Left />
-      <Right />
+      <Right icons={icons}/>
     </div>
   );
 };
@@ -25,14 +32,28 @@ const Left = () => {
       <p>Take your kids from zero to hero!</p>
       <br />
       <br />
-      <h4>Copyright©{year}</h4>
+      <h2>Copyright©{year}</h2>
     </div>
   );
 };
-const Right = () => {
+const Right = ({icons}) => {
   return (
     <div className="right">
-      <h1>Right</h1>
+        <h4>Contacts</h4>
+      <div className="icons">
+        {icons.map((icon, index) => {
+          return (
+            <div key={index} className="icon">
+              
+              <img style={{
+                width: "30px",
+                height: "30px"
+              }} src={icon} alt="icon" />
+              </div>
+          );
+        }
+        )}
+        </div>
     </div>
   );
 };
